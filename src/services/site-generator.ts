242121dfa5d026c6dd2e3a4a45fa89fa109ad79e
LoneRifle <LoneRifle@users.noreply.github.com>
2020-10-14 15:27:44 +0800

    feat: add collection generation to site-generator

--- src/services/site-generator.ts
+++ src/services/site-generator.ts
@@ -23,6 +23,32 @@ function createPages(repoName: string, pages: string[]) {
   return result
 }
 
+function createCollections(
+  repoName: string,
+  collections: { [name: string]: string[] }
+) {
+  const result = {
+    navYaml: '',
+    configYaml: '',
+    files: [] as { path: string; content: string }[],
+  }
+  if (Object.keys(collections).length) {
+    result.configYaml += 'collections:\n'
+  }
+  for (const [name, pages] of Object.entries(collections)) {
+    const humanReadableName = humanReadable(name)
+    pages.forEach((page, index) => {
+      result.files.push({
+        path: `/tmp/${repoName}/_${name}/${index}-${page}.md`,
+        content: `---\ntitle: ${humanReadableName}\npermalink: /${name}/${page}/\n---\n`,
+      })
+    })
+    result.navYaml += `  - title: ${humanReadableName}\n    collection: ${name}\n`
+    result.configYaml += `  ${name}:\n    output: true\n`
+  }
+  return result
+}
+
 export default ({
   repoName,
   pages,
@@ -31,19 +57,36 @@ export default ({
 }: {
   repoName: string
   pages: string[]
-  collections: any
+  collections: {
+    [key: string]: string[]
+  }
   resourceRoom: any
 }): void => {
   // Copy isomerpages-base to /tmp
-  fs.remove(`/tmp/${repoName}`)
+  fs.removeSync(`/tmp/${repoName}`)
   fs.copySync('./isomerpages-base', `/tmp/${repoName}`)
   const configPath = `/tmp/${repoName}/_config.yml`
   const navPath = `/tmp/${repoName}/_data/navigation.yml`
+
   const simplePagesOutput = createPages(repoName, pages)
   for (const file of simplePagesOutput.files) {
     fs.writeFileSync(file.path, file.content)
   }
+
+  const collectionsOutput = createCollections(repoName, collections)
+  for (const name of Object.keys(collections)) {
+    fs.mkdirpSync(`/tmp/${repoName}/_${name}`)
+  }
+  for (const file of collectionsOutput.files) {
+    fs.writeFileSync(file.path, file.content)
+  }
+
+  let configFile = fs.readFileSync(configPath, 'utf-8')
+  configFile += collectionsOutput.configYaml
+  fs.writeFileSync(configPath, configFile)
+
   let navFile = fs.readFileSync(navPath, 'utf-8')
   navFile += simplePagesOutput.navYaml
+  navFile += collectionsOutput.navYaml
   fs.writeFileSync(navPath, navFile)
 }
