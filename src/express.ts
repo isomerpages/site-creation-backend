c32a61745e8bf5b5b8d18c3e58f09de578e4c998
LoneRifle <LoneRifle@users.noreply.github.com>
2020-11-03 09:01:38 +0800

    feat: complete repo setup
    - Refactor GitHub handling into a separate service class
    - Ensure setup of GitHub team, branch protection, team access

--- src/express.ts
+++ src/express.ts
@@ -6,20 +6,18 @@ import { Octokit } from '@octokit/rest'
 import config from './config'
 
 import { formsg, handleSubmission } from './controllers'
+import { GitHubPublisher } from './services/github-publisher'
 
 const formSecretKey = config.get('formSecretKey')
 const githubAccessToken = config.get('githubAccessToken')
 
 const octokit = new Octokit({ auth: githubAccessToken })
+const gh = new GitHubPublisher({ octokit, githubAccessToken })
 
 const app = express()
 
 app.use(morgan('common'))
 
-app.post(
-  '/sites',
-  formsg(formSecretKey),
-  handleSubmission({ octokit, githubAccessToken })
-)
+app.post('/sites', formsg(formSecretKey), handleSubmission({ gh }))
 
 export default app
