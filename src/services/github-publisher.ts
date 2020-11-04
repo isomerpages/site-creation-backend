1eb5e01fd2bde687f9bc1599b7efd7c71e04f91b
LoneRifle <LoneRifle@users.noreply.github.com>
2020-11-04 12:02:37 +0800

    refactor(git): use isomorphic-git for pure js impl

--- src/services/github-publisher.ts
+++ src/services/github-publisher.ts
@@ -1,5 +1,8 @@
+import fs from 'fs'
+
 import { Octokit } from '@octokit/rest'
-import simpleGit from 'simple-git'
+import git from 'isomorphic-git'
+import http from 'isomorphic-git/http/node'
 
 export default ({
   octokit,
@@ -26,23 +29,48 @@ export default ({
     }),
   ])
 
-  const git = simpleGit({
-    baseDir: `/tmp/${repoName}`,
+  // Prepare git repo
+  const dir = `/tmp/${repoName}`
+  await git.init({ fs, dir, defaultBranch: 'staging' })
+  await git.add({ fs, dir, filepath: '.' })
+  await git.commit({
+    fs,
+    dir,
+    message: 'Initial commit',
+    author: {
+      name: 'isomeradmin',
+      email: 'isomeradmin@users.noreply.github.com',
+    },
+  })
+  // Push contents, staging first then master,
+  // so that staging is default branch
+  const remote = 'origin'
+  await git.addRemote({
+    fs,
+    dir,
+    remote: 'origin',
+    url: `https://github.com/isomerpages/${repoName}`,
+  })
+  await git.push({
+    fs,
+    http,
+    dir,
+    remote,
+    remoteRef: 'staging',
+    corsProxy: 'https://cors.isomorphic-git.org',
+    onAuth: () => ({ username: 'user', password: githubAccessToken }),
+  })
+  await git.push({
+    fs,
+    http,
+    dir,
+    remote,
+    remoteRef: 'master',
+    corsProxy: 'https://cors.isomorphic-git.org',
+    onAuth: () => ({ username: 'user', password: githubAccessToken }),
   })
 
-  await git
-    .init()
-    .add('.')
-    .commit('Initial commit')
-    .addRemote(
-      'origin',
-      `https://user:${githubAccessToken}@github.com/isomerpages/${repoName}`
-    )
-    .branch(['-m', 'staging'])
-    .push('origin', 'staging', { '-u': null })
-    .checkoutBranch('master', 'HEAD')
-    .push('origin', 'master', { '-u': null })
-
+  // Final touch-ups
   await Promise.all([
     octokit.repos.updateBranchProtection({
       owner: 'isomerpages',
